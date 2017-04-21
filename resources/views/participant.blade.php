ecf072ca2498e54008b0cad8f1af9d31c4866c75
Andres Rosado <andres.rosado@upr.edu>
2017-04-21 17:41:22 -0400

    Fixing the form display.

--- resources/views/participant.blade.php
+++ resources/views/participant.blade.php
@@ -7,34 +7,36 @@
 
         <form action="{{ url('participant') }}" method="POST" class="form-horizontal">
             {{ csrf_field() }}
+			<fieldset>
+				<div class="form-group">
+					<label for="name" class="col-sm-3 control-label">Name</label>
 
-            <div class="form-group">
-                <label for="name" class="col-sm-3 control-label">Name</label>
+					<div class="col-sm-6">
+						<input type="text" name="name" id="participant-name" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="email" class="col-sm-3 control-label">E-mail</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="participant-name" class="form-control">
-                </div>
+					<div class="col-sm-6">
+						<input type="text" name="email" id="participant-email" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="user_id" class="col-sm-3 control-label">User ID</label>
 
-                <label for="email" class="col-sm-3 control-label">E-mail</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="email" id="participant-email" class="form-control">
-                </div>
-
-                <label for="user_id" class="col-sm-3 control-label">User ID</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="user_id" id="participant-userid" class="form-control">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Participant
-                    </button>
-                </div>
-            </div>
+					<div class="col-sm-6">
+						<input type="text" name="user_id" id="participant-userid" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-3 col-sm-6">
+						<button type="submit" class="btn btn-default">
+							<i class="fa fa-plus"></i> Add Participant
+						</button>
+					</div>
+				</div>
+			</fieldset>
         </form>
     </div>
 @endsection
