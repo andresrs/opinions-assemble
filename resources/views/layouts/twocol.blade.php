a842fe765a57e9e3d005c24df7f71851e1deb2bd
Andres Rosado <andres.rosado@upr.edu>
2017-04-26 11:24:33 -0400

    The main body will now show up first when in mobile

--- resources/views/layouts/twocol.blade.php
+++ resources/views/layouts/twocol.blade.php
@@ -6,14 +6,14 @@
 	<br>
     <div class="container">
       <div class="row">
-        <div class="col-md-3">
-			<div class="panel panel-primary">
-				@yield('left')
-			</div>
-       </div>
-        <div class="col-md-9">
+        <div class="col-md-9 col-sm-push-3">
 			<div class="panel panel-primary">
 				@yield('content')
+			</div>
+       </div>
+        <div class="col-md-3 col-sm-pull-9">
+			<div class="panel panel-primary">
+				@yield('left')
 			</div>
        </div>
       </div>
