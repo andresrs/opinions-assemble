8265919bc70bb48653aac61fac335bd6e4ce6d71
Andres Rosado <andres.rosado@upr.edu>
2017-04-26 12:15:35 -0400

    Adding a yield for the title.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title></title>
+	<title>@yield('title')</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="apple-touch-icon" href="{{ URL::asset('apple-touch-icon.png') }}">
